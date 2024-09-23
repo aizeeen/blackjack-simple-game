305cfca8bee9025b200ae991cceaf0d69006c029
aizeeen <ihebbacha2006@gmail.com>
2024-09-23 20:56:56 +0000

    finished the sttyles and structure of the game

---
+++ index.js
@@ -0,0 +1,21 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+console.log(messageEl);
+function startGame() {
+    if (sum <= 20) {
+        message = "do you want to draw a new card";
+    } else if (sum === 21) {
+        message = "wohoo! youve got Blackjack";
+        hasBlackJack = true;
+    } else {
+        message = "youre out of the game";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
