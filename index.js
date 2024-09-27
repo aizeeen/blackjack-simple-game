3cfeae9a73809159d8dd44750569241ac6629d6d
aizeeen <ihebbacha2006@gmail.com>
2024-09-27 21:20:20 +0000

    still not completed

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
+let cards = [firstCard, secondCard]; 
+
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
-console.log(messageEl);
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+//let sumEl = document.querySelector("#sum-el");
+
 function startGame() {
+    return renderGame();
+}
+
+function renderGame() {
+    sumEl.textContent = "sum: " + sum;
+    cardsEl.textContent = "cards: " + cards[0] + " " + cards[1];
+
     if (sum <= 20) {
         message = "do you want to draw a new card";
     } else if (sum === 21) {
@@ -20,4 +32,10 @@ function startGame() {
     messageEl.textContent = message;
 }
 
-// tommorow i gotta finish the cards and sum parts
+function newCard() {
+    console.log("drawing new card");
+    let Card = 7;
+
+    sum += Card;
+    renderGame();
+}
